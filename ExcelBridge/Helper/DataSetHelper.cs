4236006587d9fc27784e0c5f71d4c88b93a9bdf5
Gaurang Gupta <gaurangfgupta@gmail.com>
2016-04-06 12:03:54 +0530

    Dataset helpers
    A Helper namespace with Dataset helper created. This helps in performing
    few common validation and cleaning up functions on DataSets.
    Unit tests in place.
    > Documentation and probably architecture redesign pending.

---
+++ ExcelBridge/Helper/DataSetHelper.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ExcelBridge.Helper
+{
+    public static class DataSetHelper
+    {
+        public static Dictionary<string, bool> CheckColumnsConsistency(DataSet dataset, bool ignoreColumnOrder = true)
+        {
+            Dictionary<string, bool> validationSummary = new Dictionary<string, bool>();
+            if (dataset.Tables.Count > 1)
+            {
+                #region Prepare reference list of columns from first table of dataset. Used later to compare columns of other tables.
+                List<string> referenceColumns = getColumnsOfDataTable(dataset.Tables[0], ignoreColumnOrder);
+                #endregion
+
+                foreach (DataTable table in dataset.Tables)
+                {
+                    bool consistencySwitch = false;
+                    List<string> columnsOfTargetTable = getColumnsOfDataTable(table, ignoreColumnOrder);
+
+                    if (referenceColumns.Count == columnsOfTargetTable.Count)
+                    {
+                        consistencySwitch = referenceColumns.SequenceEqual(columnsOfTargetTable);
+                    }
+
+                    validationSummary.Add(table.TableName, consistencySwitch);
+                }
+            }
+            return validationSummary;
+        }
+
+        private static List<string> getColumnsOfDataTable(DataTable sourceTable, bool sort = false)
+        {
+            List<string> columns = new List<string>();
+            foreach (DataColumn col in sourceTable.Columns)
+            {
+                columns.Add(col.ColumnName);
+            }
+            if (sort)
+            {
+                columns.Sort();
+            }
+            return columns;
+        }
+
+        public static Dictionary<string, int> RemoveBlankRows(ref DataSet dataSet)
+        {
+            Dictionary<string, int> deletionSummary;
+            deletionSummary = new Dictionary<string, int>();
+
+            foreach (DataTable table in dataSet.Tables)
+            {
+                int deletedRowsCount = 0;
+                List<DataRow> rowsToDelete = new List<DataRow>();
+
+                foreach (DataRow row in table.Rows)
+                {
+                    //Count blank columns in row
+                    int blankColumnCount = 0;
+                    foreach (DataColumn col in table.Columns)
+                    {
+                        if (string.IsNullOrWhiteSpace(row[col.ColumnName].ToString()))
+                        {
+                            blankColumnCount++;
+                        }
+                    }
+                    //If all columns blank, collect row in deletion list
+                    if (blankColumnCount == table.Columns.Count)
+                    {
+                        rowsToDelete.Add(row);
+                        deletedRowsCount++;
+                    }
+                }
+
+                //Delete collected rows
+                foreach (DataRow rowToDelete in rowsToDelete)
+                {
+                    table.Rows.Remove(rowToDelete);
+                }
+
+                deletionSummary.Add(table.TableName, deletedRowsCount);
+            }
+            return deletionSummary;
+        }
+
+    }
+}
---
+++ ExcelBridgeTest/DatatableValidatorTest.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExcelBridge.Helper;
+using System.Collections.Generic;
+
+namespace ExcelBridgeTest
+{
+    [TestClass]
+    public class DatatableValidatorTest
+    {
+        [TestMethod]
+        public void CheckColumnsConsistency()
+        {
+            DataSet _dataset;
+            _dataset = new DataSet();
+            Dictionary<string, bool> resultSummary;
+
+            #region Create reference table to compare other tables with
+            //Create reference table and add to dataset
+            DataTable referenceTable = new DataTable("Reference Table");
+            for (int i = 1; i < 6; i++)
+            {
+                referenceTable.Columns.Add(string.Format("Column{0}", i));
+            }
+            _dataset.Tables.Add(referenceTable);
+            //Check consistency
+            resultSummary = DataSetHelper.CheckColumnsConsistency(_dataset);
+            //Assert expected results
+            Assert.AreEqual(resultSummary.Count, 0);
+            //Release objects
+            #endregion
+
+            #region Check equal table
+            //Create target table and add to dataset
+            DataTable equalTable = new DataTable("Equivalent Table");
+            for (int i = 1; i < 6; i++)
+            {
+                equalTable.Columns.Add(string.Format("Column{0}", i));
+            }
+            _dataset.Tables.Add(equalTable);
+            //Check consistency ignoring column order
+            resultSummary = DataSetHelper.CheckColumnsConsistency(_dataset);
+            //Assert expected results
+            foreach (bool columnConsistencyResult in resultSummary.Values)
+            {
+                Assert.IsTrue(columnConsistencyResult);
+            }
+            //Check consistency considering column order
+            resultSummary = DataSetHelper.CheckColumnsConsistency(_dataset,false);
+            //Assert expected results
+            foreach (bool columnConsistencyResult in resultSummary.Values)
+            {
+                Assert.IsTrue(columnConsistencyResult);
+            }
+            //Release objects
+            equalTable = null;
+            _dataset.Tables.RemoveAt(1);
+            #endregion
+
+            #region Check table with same columns but different order
+            //Create target table and add to dataset
+            DataTable differentOrderTable = new DataTable("Different order table");
+            for (int i = 5; i > 0; i--)
+            {
+                differentOrderTable.Columns.Add(string.Format("Column{0}", i));
+            }
+            _dataset.Tables.Add(differentOrderTable);
+            //Check consistency ignoring column order
+            resultSummary = DataSetHelper.CheckColumnsConsistency(_dataset);
+            //Assert expected results
+            Assert.IsTrue(resultSummary[differentOrderTable.TableName]);
+            //Check consistency considering column order
+            resultSummary = DataSetHelper.CheckColumnsConsistency(_dataset,false);
+            //Assert expected results
+            Assert.IsFalse(resultSummary[differentOrderTable.TableName]);
+            //Release objects
+            differentOrderTable = null;
+            _dataset.Tables.RemoveAt(1);
+            #endregion
+
+            #region Check table with different column count
+            //Create target table and add to dataset
+            DataTable differentCountTable = new DataTable("Different column count table");
+            for (int i = 1; i < 7; i++)
+            {
+                differentCountTable.Columns.Add(string.Format("Column{0}", i));
+            }
+            _dataset.Tables.Add(differentCountTable);
+            //Check consistency
+            resultSummary = DataSetHelper.CheckColumnsConsistency(_dataset);
+            //Assert expected results
+            Assert.IsFalse(resultSummary[differentCountTable.TableName]);
+            //Release objects
+            differentCountTable = null;
+            _dataset.Tables.RemoveAt(1);
+            #endregion
+
+            #region Check table with different columns
+            //Create target table and add to dataset
+            DataTable differentColumnsTable = new DataTable("Different column count table");
+            for (int i = 1; i < 5; i++)
+            {
+                differentColumnsTable.Columns.Add(string.Format("Column{0}", i));
+            }
+            differentColumnsTable.Columns.Add(string.Format("Column{0}", 7));
+            _dataset.Tables.Add(differentColumnsTable);
+            //Check consistency ignoring column order
+            resultSummary = DataSetHelper.CheckColumnsConsistency(_dataset);
+            //Assert expected results
+            Assert.IsFalse(resultSummary[differentColumnsTable.TableName]);
+            //Check consistency considering column order
+            resultSummary = DataSetHelper.CheckColumnsConsistency(_dataset,false);
+            //Assert expected results
+            Assert.IsFalse(resultSummary[differentColumnsTable.TableName]);
+            //Release objects
+            differentColumnsTable = null;
+            _dataset.Tables.RemoveAt(1);
+            #endregion
+        }
+    }
+
+}
